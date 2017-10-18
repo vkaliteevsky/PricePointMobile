ff1ad2cbfda7ca42f573ea04b0212701b0fd4bd4
Vasiliy Kaliteevskiy <vkalit@gmail.com>
2017-10-18 22:03:22 +0300

    18.102017

--- app/src/main/java/com/nerv/pricepoint/TaskPageFragment.java
+++ app/src/main/java/com/nerv/pricepoint/TaskPageFragment.java
@@ -1,12 +1,32 @@
 package com.nerv.pricepoint;
 
+import android.animation.LayoutTransition;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Transition;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.StackView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.crosswall.lib.coverflow.CoverFlow;
+import me.crosswall.lib.coverflow.core.PagerContainer;
 
 /**
  * Created by NERV on 17.10.2017.
@@ -59,10 +79,103 @@ public class TaskPageFragment extends CustomFragment implements View.OnClickList
         }
     }
 
+    public static class PhotoStackAdapter extends BaseAdapter {
+        LayoutInflater inflater;
+
+        public PhotoStackAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return "";
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            View itemView = view;
+
+            if (itemView == null) {
+                itemView = inflater.inflate(R.layout.sv_photos_item, parent, false);
+            }
+
+            return itemView;
+        }
+    }
+
+    public static class PhotoItemFragment extends Fragment {
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.sv_photos_item, null);
+        }
+    }
+
+    public static class PhotoItemFragmentAdapter extends FragmentPagerAdapter {
+        private List<Fragment> fragments;
+
+        public PhotoItemFragmentAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+
+        LayoutInflater inflater;
+
+        public MyPagerAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = inflater.inflate(R.layout.sv_photos_item,null);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return (view == object);
+        }
+    }
+
     private View view;
     private View commentBtn;
     private RecyclerView promosRV;
     private PromoRecyclerAdapter promoRecyclerAdapter;
+    PagerContainer pc;
 
     private MainActivity main;
 
@@ -85,6 +198,50 @@ public class TaskPageFragment extends CustomFragment implements View.OnClickList
 
         commentBtn = view.findViewById(R.id.commentBtn);
         commentBtn.setOnClickListener(this);
+
+        List<Fragment> fs = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            fs.add(new PhotoItemFragment());
+        }
+
+        /*photosSV = (FlippableStackView) view.findViewById(R.id.photosSV);
+        photosSV.initStack(4, StackPageTransformer.Orientation.HORIZONTAL, 0.53f, 0.46f, 0.46f, StackPageTransformer.Gravity.CENTER);
+        photosSV.setAdapter(new PhotoItemFragmentAdapter(main.getSupportFragmentManager(), fs));*/
+
+        pc =  (PagerContainer) view.findViewById(R.id.photosPC);
+        pc.setOverlapEnabled(true);
+        pc.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+
+        final ViewPager pager = pc.getViewPager();
+        pager.setOffscreenPageLimit(4);
+        pager.setAdapter(new PhotoItemFragmentAdapter(main.getSupportFragmentManager(), fs));
+
+        new CoverFlow.Builder().with(pager)
+                .scale(0.3f)
+                .pagerMargin(main.getResources().getDimensionPixelSize(R.dimen.overlapMargin))
+                .spaceSize(0)
+                .build();
+
+
+        LinearLayout pricesLayout = (LinearLayout) view.findViewById(R.id.prices);
+        LayoutTransition lt = pricesLayout.getLayoutTransition();
+        lt.enableTransitionType(LayoutTransition.CHANGING);
+
+        /*pager.post(new Runnable() {
+            @Override public void run() {
+                RelativeLayout v = (RelativeLayout) pager.getAdapter().instantiateItem(pager, 0);
+                ViewCompat.setElevation(v, 8.0f);
+            }
+        });*/
+
+        /*pager.post(new Runnable() {
+            @Override public void run() {
+                RelativeLayout view = (RelativeLayout) pager.getAdapter().instantiateItem(pager, 0);
+                ViewCompat.setElevation(view, 8.0f);
+            }
+        });*/
 
         return view;
     }
